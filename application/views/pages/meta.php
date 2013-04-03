0bd503cea4cb3884f1b755d4ee8d27c17f964e9a
Pete <pete.albrecht@gmail.com>
2013-04-02 22:57:19 -0600

    edited and reinstated meta page

--- application/views/pages/meta.php
+++ application/views/pages/meta.php
@@ -1,11 +1,6 @@
 <h2>About this site</h2>
-<p>This site was created using the Codeigniter MVC framework. Pages are
-constructed from "fragments" or separate views such as the header, navigation
-bar, content and source code views. Static pages are served up using a
-controller which loads the requested page into the content view.</p>
-<p>The projects page has its own controller which loads the requested project
-or defaults to the home page if no project is requested. The contact page was 
-created using Codeigniter's form helper, form validation and email classes, and 
-xss filter</p>
-<p>You can view the back-end controller and model (if applicable) for any page
-by clicking the box near the bottom. Feel free to have a look around!</p>
+<p>This site was created using the Codeigniter 2.1.3 MVC framework. You can view 
+the back-end controller and model (if applicable) for any page by clicking the box 
+near the bottom. You can browse the source code for the site 
+<a href="https://bitbucket.org/pdizz/pdizz-site/src">here.</a> Feel free to have 
+a look around!</p>

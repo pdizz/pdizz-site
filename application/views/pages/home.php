66586a5d2d8d0b3edd0caabce0dd757c6d529040
Pete <pete.albrecht@gmail.com>
2013-04-02 22:46:33 -0600

    Home page done for now...

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,9 +1,24 @@
 <h1>Pete Albrecht - Web Developer</h1>
 <h2>About Me</h2>
+<p>I am a geek, a programmer, a gamer, a media junkie, and an outdoor enthusiast. 
+I love programming and the power it gives me to create something from nothing. 
+I'm a student of algorithms and I'm fascinated with solving impossibly large problems 
+using clever techniques. </p>
+<p>When I'm not in the office I love to get outdoors and hit the trail. I've written 
+some of my best code in in my head while walking my dog. I like kayaking and mountain 
+biking, and I enjoy the mental and physical challenge of archery and marksmanship. 
+Whether it's new technologies or new trails I'm always exploring.</p>
+<h2>Career</h2>
 <p>I started my web development career after working for 10 years in broadcasting 
 and print media. I've worked as a photographer, printer, recording engineer, 
 even as a director for a local news broadcast. While interesting, all other forms of 
 media pale in comparison to the power of the Internet to inform and entertain.</p>
+<p>Programming makes me feel like a mage with the power to solve problems, save time, 
+waste time, entertain, and create new worlds out of thin air. My first exposure 
+to programming was through the book <a href="http://eloquentjavascript.net/">Eloquent JavaScript</a>, 
+which I highly recommend. I was instantly hooked. Before long I was using my 
+new super powers to solve puzzles on <a href="http://projecteuler.net/">Project Euler</a> 
+and change the text on webpages to funny and inappropriate things. </p>
 <p>Like many others, my first project was a pet project I developed to solve a 
 particular problem in my life. I'm an avid gamer and one of my favorite series is 
 "The Elder Scolls" RPG series. I designed a database of alchemy ingredients and 
@@ -14,4 +29,12 @@ and list all possible potion recipes given the ingredients I had in my inventory
 Through the project I not only learned about what's involved in crafting a web 
 application, but also developed an affinity for database management and the power 
 of SQL.</p>
+<p>While working for a photography company I found new ways to apply my programming 
+knowledge to solving some interesting. I started writing small Photoshop scripts 
+using JavaScript for repetitive tasks like generating proofs and resizing/cropping 
+images to different aspect ratios. Eventually I was able to develop a 
+<a href="<?php echo base_url(); ?>/projects/bin-packing">bin packing algorithm</a> 
+to solve the problem of packing different size images to fixed-size sheets of paper. 
+Being able to use an fascinating algorithm to solve a unique problem was an experience 
+I'll never forget.</p>
 

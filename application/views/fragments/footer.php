ffb1103033474edf65c2640ad8d3575139f4b131
Pete <pete.albrecht@gmail.com>
2013-04-04 22:17:41 -0600

    fixed resume and footer css issues

--- application/views/fragments/footer.php
+++ application/views/fragments/footer.php
@@ -1,7 +1,8 @@
 <!-- beginning of footer view -->
 
-        </div> <!-- end of #pdizz namespace div -->
-
         <div id="copyright">&copy; <?php echo date("Y"); ?> - Pete Albrecht</div>
+        
+        </div> <!-- end of #pdizz namespace div -->
+        
     </body>
 </html>

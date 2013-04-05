3e5819fa1c15dbe8d4ac012da29c1c10e01d0d09
Pete <pete.albrecht@gmail.com>
2013-04-05 13:05:57 -0600

    added description and sample images to bin-packing project

--- application/views/pages/projects.php
+++ application/views/pages/projects.php
@@ -1,11 +1,13 @@
 <h1>Projects</h1>
-<h2><a href="<?php echo base_url(); ?>projects/bin-packing">Bin-packing for Photographers</a></h2>
-<p></p>
+<h2><a href="<?php echo base_url(); ?>projects/bin-packing">Bin-packing for School Pictures</a></h2>
+<img src="<?php echo base_url(); ?>images/bin-demo1.jpg" alt="Sample image 5x7 with two 3x5" />
+<img src="<?php echo base_url(); ?>images/bin-demo2.jpg" alt="Sample image 4 wallets and 8 mini-wallets" />
 
 <h2><a href="https://github.com/pdizz/reddit.php">Reddit API client in PHP</a></h2>
-<p></p>
+<p>An API client in object-oriented PHP. Interfaces with <a href="http://www.reddit.com/">reddit.com</a>
+API and parses JSON data structure into easy to use PHP classes. </p>
 
 <!--
-<h2><a href="<?php echo base_url(); ?>projects/alchemy">Elder Scrolls Alchemy Workshop</a></h2>
+<h2><a href="https://github.com/pdizz/Alchemy">Elder Scrolls Alchemy Workshop</a></h2>
 <p></p>
 -->
--- application/views/projects/bin-packing.php
+++ application/views/projects/bin-packing.php
@@ -1,10 +1,31 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/demo.css" />
 
-<div id="content">        
+<div id="content">   
+    
+    <h2>Bin Packing for School Pictures - Demo</h2>
+    
+    <p>Bin packing algorithm derived from 
+    <a href="https://github.com/jakesgordon/bin-packing">github.com/jakesgordon/bin-packing</a>
+    and adapted to pack to multiple growing bins with a fixed maximum size. Full 
+    source code available at <a href="https://github.com/pdizz/Packager">github.com/pdizz/Packager</a>.</p>
+    <p>This algorithm will pack multiple images to fit on variable-length sheets
+    printed on roll-paper. It is used in printing school picture packages such as 
+    a sheet of wallets, or two 5x7 pictures on a single 10x7 sheet. Because the 
+    length of the sheets varies the size of the "bin" or sheet must grow to fit 
+    the images as they're added without wasting extra space or requiring the paper 
+    to be cut by hand.</p>
+    <p>This prototype algorithm was translated into C# and used in our production 
+    software with great success.</p>
+    
+    <img src="<?php echo base_url(); ?>images/bin-demo1.jpg" alt="Sample image 5x7 with two 3x5" />
+    <img src="<?php echo base_url(); ?>images/bin-demo2.jpg" alt="Sample image 4 wallets and 8 mini-wallets" />
+    
+    <h2>Demo</h2>
+    
     <div class='blocks'>
         <label for='blocks'>Blocks: W x H [x N] - </br>
             <pre>
-    For the purposes of this demo sizes must be entered from largest to smallest.
+    For best results, sizes must be entered from largest to smallest.
     This will be taken care of using a sort algorithm or through the UI eventually.</br>
     Maximum sheet size is 100x80</br>
     8x10       = '100x80'
@@ -18,7 +39,7 @@
 100x80
 50x70
 50x35x2
-25x35x4
+25x35x8
 25x17.5x8
         </textarea>
     </div>

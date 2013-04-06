8cfaccfc871b04bf0a0520d784d246544b6f45f5
Pete <pete.albrecht@gmail.com>
2013-04-05 23:47:13 -0600

    moved bin-packing project description to projects home page

--- application/views/projects/bin-packing.php
+++ application/views/projects/bin-packing.php
@@ -3,23 +3,7 @@
 <div id="content">   
     
     <h2>Bin Packing for School Pictures - Demo</h2>
-    
-    <p>Bin packing algorithm derived from 
-    <a href="https://github.com/jakesgordon/bin-packing">github.com/jakesgordon/bin-packing</a>
-    and adapted to pack to multiple growing bins with a fixed maximum size. Full 
-    source code available at <a href="https://github.com/pdizz/Packager">github.com/pdizz/Packager</a>.</p>
-    <p>This algorithm will pack multiple images to fit on variable-length sheets
-    printed on roll-paper. It is used in printing school picture packages e.g. a 
-    sheet of wallets. Because the length of the sheets varies the size of the "bin" 
-    or sheet must grow to fit the images as they're added without wasting extra space 
-    or requiring the paper to be cut by hand.</p>
-    <p>This prototype algorithm was translated into C# and used in our production 
-    software with great success.</p>
-    
-    <img src="<?php echo base_url(); ?>images/bin-demo1.jpg" alt="Sample image 5x7 with two 3x5" />
-    <img src="<?php echo base_url(); ?>images/bin-demo2.jpg" alt="Sample image 4 wallets and 8 mini-wallets" />
-    
-    <h2>Demo</h2>
+    <p><a href="https://github.com/pdizz/Packager">github.com/pdizz/Packager</a></p>
     
     <div class='blocks'>
         <label for='blocks'>Blocks: W x H [x N] - </br>

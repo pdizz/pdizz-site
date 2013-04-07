3eed2673bfeaa5c7654d60f35620ea8c4661f1fd
Pete <pete.albrecht@gmail.com>
2013-04-06 22:20:40 -0600

    added google analytics script

--- application/views/fragments/header.php
+++ application/views/fragments/header.php
@@ -7,6 +7,16 @@
         <meta name="keywords" content="PHP, JavaScript, MySQL, HTML, CSS, Web, Developer, Development" />
         <meta http-equiv="content-type" content="text/html;charset=utf-8">
 
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-39863854-1', 'pdizz.com');
+            ga('send', 'pageview');
+
+        </script>
         <script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
         <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script type="text/javascript" src="<?php echo base_url(); ?>js/source_code.js"></script>
